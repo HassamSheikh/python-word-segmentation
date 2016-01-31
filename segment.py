c778e0bd8d0cfce78e8c3f06957927480544d066
Hassam Sheikh <hassam.sheikh@7vals.com>
2016-01-31 22:58:20 +0500

    Do not write code at 12 in night

--- segment.py
+++ segment.py
@@ -96,7 +96,7 @@ def set_segmentation_data(segment_data, **kwargs):
 def segment(text, **kwargs):
     factor                        = kwargs.get('factor') if kwargs.get('factor') > 1 else 1
     if bool(unigram_counts) is False or 'segment_data' in kwargs:
-        set_segmentation_data(kwargs.get('segment_data', factor = factor))
+        set_segmentation_data(kwargs.get('segment_data'), factor = factor)
     
     @memoize
     def search(text, prev='<S>'):
